c2cba90626b32a66a4f07fcf60b3e113f5669ec3
lwena27 <lwena027@gmail.com>
2025-01-04 22:29:09 +0300

    up to 57

---
+++ README.md
@@ -0,0 +1,1 @@
+you may use this code the way u want. just give me a sts for the work i didi
---
+++ src/CSE.java
@@ -0,0 +1,10 @@
+public class CSE {
+    // creating a default constructor
+    CSE(){
+        System.out.println("Telecom class created");
+    }
+    public static void main(String[] args) {
+        //calling a default constructor
+        CSE b = new CSE();
+    }
+}
