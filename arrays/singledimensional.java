bd1439404fcca7aa388d13726b445cac46f4e3c7
lwena27 <lwena027@gmail.com>
2025-01-07 22:30:48 +0300

    ee

--- arrays/singledimensional.java
+++ arrays/singledimensional.java
@@ -1,5 +1,6 @@
 class singledimensional{
     public static void main(String args[]){
+        //allay of stlings
         String friend[]={"juma","James","Neema"};
         System.out.println(friend[0]);
         System.out.println(friend.length);
