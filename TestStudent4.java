057dba3d85d9d6ecad4dba44e7a6ade4ddb90ff8
Lwena <165172325+LWENA27@users.noreply.github.com>
2025-01-28 22:59:16 +0300

    Update TestStudent4.java

--- TestStudent4.java
+++ TestStudent4.java
@@ -13,7 +13,7 @@ class Students1{
     public static void main(String args[]){
         Students1 s1=new Students1();
         Students1 s2=new Students1();
-    s1.insertRecord(200,"Juma");
+    s1.insertRecord(200,"Juma");//pass parameters 
     s2.insertRecord(300,"Neema");
     s1.displayInformation();
     s2.displayInformation();
